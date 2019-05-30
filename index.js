e3c18f15cfa788cf68813236aea25cb27cc65fce
grobira <grobira2@gmail.com>
2019-05-30 00:07:04 -0300

    organizaçao das pastas

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require( 'express');
 const bodyparser = require('body-parser');
-const expressLayouts = require('express-ejs-layouts');
 const router = require('./router');
 
 const app = express();
-app.use(expressLayouts);
 app.set('view engine', 'ejs');
+app.set('views', './views')
 app.use(bodyparser.urlencoded({ extended: true }));
-app.use(express.static('./css'));
+app.use(express.static('./static'));
 
 app.use('/', router);
 
 app.listen(process.env.PORT || 8080, () => {
+  console.log('test')
   console.log('Listening to port 8080');
 });
--- router.js
+++ router.js
@@ -4,8 +4,7 @@ const ejs = require('ejs');
 const people = ['geddy', 'neil', 'alex'];
 
 router.get('/', (req, res) =>{
-    const html = ejs.render('<%= people.join(", "); %>', {people: people});
-    res.send(html);
+    res.render('index', {people});
 })
 
 module.exports = router;
